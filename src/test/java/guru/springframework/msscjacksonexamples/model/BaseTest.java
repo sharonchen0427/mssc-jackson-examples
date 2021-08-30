3d40c20f38e0a957ca2fb83e9f4cea1841603922
-chenshumei <-chensharon0427@gmail.com>
2021-08-29 22:26:35 -0400

    json test -json = string; video 106

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BaseTest.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class BaseTest {
+    BeerDto getDto(){
+        return BeerDto.builder()
+                .beerName("beerName")
+                .beerStyle("Ale")
+                .id(UUID.randomUUID())
+                .createdDate(OffsetDateTime.now())
+                .lastUpdatedDate(OffsetDateTime.now())
+                .upc(12345L)
+                .price(new BigDecimal("2.99"))
+                .build();
+    }
+}
---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@JsonTest
+class BeerDtoTest extends BaseTest {
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSerializeDto() throws JsonProcessingException {
+        BeerDto beerDto=getDto();
+        String jsonString =objectMapper.writeValueAsString(beerDto);
+        System.out.println(jsonString);
+    }
+
+    @Test
+    void testDeserialize() throws IOException {
+        String jsonString="{\"id\":\"639c00ff-9993-4ec1-af12-8277c85f93bb\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2019-06-02T16:35:58.321001-04:00\",\"lastUpdatedDate\":\"2019-06-02T16:35:58.321872-04:00\"}";//json---> string
+        BeerDto beerDto=objectMapper.readValue(jsonString,BeerDto.class);
+        System.out.println(beerDto);
+    }
+
+}
